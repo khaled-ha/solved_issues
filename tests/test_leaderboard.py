73f092459931881b9be48dfa242b912ba2dcc777
khaledha <khaledhadjali1@gmail.com>
2022-10-22 22:10:12 +0100

    adding test for leadbord prob

--- tests/test_leaderboard.py
+++ tests/test_leaderboard.py
@@ -6,23 +6,24 @@ def convert_to_int(data):
 
 @pytest.fixture
 def get_ranking_fixture():
-    with open('tests/fixtures/ranking.csv', 'r') as f:                
+    with open('tests/fixtures/2000_data/ranking.csv', 'r') as f:                
         return f.read().split(',') 
 
 @pytest.fixture
 def get_player_ranks():
-    with open('tests/fixtures/player.csv') as f:
+    with open('tests/fixtures/2000_data/player.csv') as f:
       return f.read().split(',')
 
 @pytest.fixture
 def get_expected_output():
-    with open('tests/fixtures/expected_out_puts.txt') as f:
+    with open('tests/fixtures/2000_data/expected_out_puts.txt') as f:
         return f.read().replace('\n', ',').split(',')
 
 def get_data(path):
     with open(path, 'r') as f:
         data = f.readlines()
     return data
+
 
 
 @pytest.mark.parametrize('ranking, player, expected_res', [(pytest.lazy_fixture("get_ranking_fixture"), pytest.lazy_fixture("get_player_ranks"), pytest.lazy_fixture("get_expected_output"))])
