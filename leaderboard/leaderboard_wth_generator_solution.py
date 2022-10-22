a04eaf1a9b95c69487a685bf5b33631c89f1ebad
khaledha <khaledhadjali1@gmail.com>
2022-10-22 16:30:44 +0100

    adding test for leadbord prob

---
+++ leaderboard/leaderboard_wth_generator_solution.py
@@ -0,0 +1,29 @@
+def get_new_position(player_position, ranked):
+    i = len(ranked) - 1
+    while i >= 1:
+        if player_position < ranked[i]:
+            yield i +2
+        if player_position == ranked[i]:
+            yield i + 1
+        i = i - 1
+    yield i +1
+
+def get_player_leaderborad(ranked, player):
+    ranked = list(set(ranked))
+    ranked.sort(reverse=True)
+    return [next(get_new_position(p, ranked)) for p in player]
+
+
+if __name__ == '__main__':
+
+    ranked_count = int(input().strip())
+
+    ranked = list(set(list(map(int, input().rstrip().split()))))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    ranked.sort(reverse=True)
+    #ranked = sorted(ranked, reverse=True)
+    result =[next(get_new_position(p, ranked)) for p in player]
---
+++ tests/test_leaderboard.py
@@ -0,0 +1,34 @@
+import pytest
+from ..leaderboard.leaderboard_wth_generator_solution import get_player_leaderborad
+
+def convert_to_int(data):
+    return [int(x) for x in data]
+
+@pytest.fixture
+def get_ranking_fixture():
+    with open('tests/fixtures/ranking.csv', 'r') as f:                
+        return f.read().split(',') 
+
+@pytest.fixture
+def get_player_ranks():
+    with open('tests/fixtures/player.csv') as f:
+      return f.read().split(',')
+
+@pytest.fixture
+def get_expected_output():
+    with open('tests/fixtures/expected_out_puts.txt') as f:
+        return f.read().replace('\n', ',').split(',')
+
+def get_data(path):
+    with open(path, 'r') as f:
+        data = f.readlines()
+    return data
+
+
+@pytest.mark.parametrize('ranking, player, expected_res', [(pytest.lazy_fixture("get_ranking_fixture"), pytest.lazy_fixture("get_player_ranks"), pytest.lazy_fixture("get_expected_output"))])
+def test_leadernboard(ranking, player, expected_res):
+    ranking = convert_to_int(ranking)
+    player = convert_to_int(player)
+    expected_res = convert_to_int(expected_res)
+    res = get_player_leaderborad(ranking, player)
+    assert res == expected_res
