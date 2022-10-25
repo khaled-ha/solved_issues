a391e622435b04601313a54ee940b32066b46c0c
khaledha <khaledhadjali1@gmail.com>
2022-10-25 09:37:08 +0100

    calculating time diiference between two date

--- timediiference.py
+++ timediiference.py
@@ -13,6 +13,11 @@ def time_delta(t1, t2):
     t1_date = datetime.strptime(t1, '%a %d %b %Y %H:%M:%S %z')
     t2_date = datetime.strptime(t2, '%a %d %b %Y %H:%M:%S %z')
     delta = abs(t1_date - t2_date)
+    #t1_ts = datetime.timestamp(t1_date)
+    #t2_ts = datetime.timestamp(t2_date)
+    
+    #if int(t1.split(' ')[1]) == int(t2.split(' ')[1]):
+    #    return str(int(t1_ts - t2_ts))
     if delta.days >= 1:
         return str(24 * 3600 * delta.days + delta.seconds)
     return str(delta.seconds)
