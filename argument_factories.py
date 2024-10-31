c7cd258ad56fa60f92cffb0f6fa5b86e15148aaa
khaled hadj ali <khaledhadjali1@gmail.com>
2024-10-31 22:47:27 +0100

    mutable default argument

--- argument_factories.py
+++ argument_factories.py
@@ -19,7 +19,8 @@ class DefaultValues(BaseModel):
     a :list = [] # pydantic handle this automaticly by making a deep copy of [] and it's not the same case of dataclass
     # This is will be evaluated wheb class is parsed not when instance is created
     b: datetime = datetime.now() # in this case is interpreted at compiling time so in all instances it's gonna be the same value
-    c: datetime = Field(default_factory= lambda: datetime.now()) # that's the best practice to do not 
+    c: datetime = Field(default_factory= lambda: datetime.now()) # that's the best practice to do not
+    d: dict = {}
 
 
 if __name__ == '__main__':
@@ -37,8 +38,10 @@ if __name__ == '__main__':
 
     print('testing default value list and calculated value')
     default_value1 = DefaultValues()
-    print(default_value1.a, default_value1.b, default_value1.c)
+    default_value1.d['1'] = 1
+    default_value1.a.append(1)
+    print(default_value1.a, default_value1.b, default_value1.c, default_value1.d)
     default_value2 = DefaultValues()
-    print(default_value2.a, default_value2.b, default_value2.c)
+    print(default_value2.a, default_value2.b, default_value2.c, default_value2.d)
     default_value3 = DefaultValues()
-    print(default_value3.a, default_value3.b, default_value3.c)
+    print(default_value3.a, default_value3.b, default_value3.c, default_value3.d)
