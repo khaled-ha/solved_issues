fe6a2fb9f17833c3b2adf77b4d4a4a8fc2e578f8
khaled hadj ali <khaledhadjali1@gmail.com>
2024-10-31 22:43:34 +0100

    mutable default argument

--- argument_factories.py
+++ argument_factories.py
@@ -17,6 +17,7 @@ def defautl_arg_dict(data: dict = {}):
 
 class DefaultValues(BaseModel):
     a :list = [] # pydantic handle this automaticly by making a deep copy of [] and it's not the same case of dataclass
+    # This is will be evaluated wheb class is parsed not when instance is created
     b: datetime = datetime.now() # in this case is interpreted at compiling time so in all instances it's gonna be the same value
     c: datetime = Field(default_factory= lambda: datetime.now()) # that's the best practice to do not 
 
